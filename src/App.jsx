cc1e2aae7c9bd7cea69db2ab0a4521ebe5264dcd
Marco <marcodutto1994@gmail.com>
2024-12-08 15:44:44 -0300

    se avanza solucion de overflow en baja resolucion

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Tecnicos from "./components/Pages/Tecnicos/";
 import Cursos from "./components/Pages/Cursos";
 import CampusVirtual from "./components/Pages/CampusVirtual";
 import Noticias from "./components/Pages/Noticias";
-import Inicio from "./components/Pages/Inicio";
+import Inicio from "./components/Pages/inicio";
 
 function App() {
 
@@ -17,6 +17,7 @@ function App() {
         <title>ASATEC</title>
         <link rel="icon" type='image/x-icon' href='./public/img/ASATEC-logo.png' />
 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Anton&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
--- src/components/Pages/inicio.jsx
+++ src/components/Pages/inicio.jsx
@@ -1,36 +1,54 @@
 // import React from "react";
-import './inicio.css';
-import logo from '/public/img/ASATEC-logo.png';
-import aes from '/public/img/AES-logo.png';
+import "./inicio.css";
+import logo from "/public/img/ASATEC-logo.png";
+import aes from "/public/img/AES-logo.png";
 
 const inicio = () => {
   return (
     <div>
-
-      <div className="inicio-background">
-        <div className="container roboto">
+      <div className="container inicio-background">
+        <div className="box roboto">
           <h1>BIENVENIDO</h1>
-          <p className="text-box">Únete a nuestra <b>comunidad</b> y accede a una amplia red de contactos. Aumenta tus <b>conocimientos</b>, mejora tus habilidades y expande tus oportunidades laborales.</p>
-          <p><b>¡En AES+ siempre hay algo nuevo que aprender!</b></p>
-          <a href="" className='text-box boton'>Conoce nuestros cursos</a>
+          <p className="text-box">
+            Únete a nuestra <b>comunidad</b> y accede a una amplia red de
+            contactos. Aumenta tus <b>conocimientos</b>, mejora tus habilidades
+            y expande tus oportunidades laborales.
+          </p>
+
+          <p>
+            <b>¡En AES+ siempre hay algo nuevo que aprender!</b>
+          </p>
+          <a href="" className="text-box boton">
+            Conoce nuestros cursos
+          </a>
         </div>
       </div>
 
-      <div className="inicio-background-blue">
-        <div className='container-transp'>
+      <div className="container inicio-background-blue">
+        <div className="box-transp">
           <img src={logo} alt="logo de ASATEC" />
           <img src={aes} alt="logo de AES" />
         </div>
       </div>
 
-      <div className="inicio-background-l-blue ">
-        <div className='container-transp roboto'>
+      <div className="container inicio-background-l-blue ">
+        <div className="box-transp roboto">
           <h2>Asociación Argentina de Técnicos en telefonía móvil- ASATEC.</h2>
-          <p>Nuestra asociación <b>nació hace 15 años</b> con la misión de promover el desarrollo profesional de los técnicos de celulares. Creemos en la importancia de la <b>formación continua</b> y la <b>actualización</b> de conocimientos. Por eso, creamos <b>ASATEC Estudios Superiores (AES+)</b>, que ofrece una amplia gama de cursos y especializaciones. Contamos con certificados no oficiales que garantizan la <b>calidad</b> de nuestros servicios y el <b>compromiso</b> con la excelencia.</p>
-          <a href="/nosotros" className='boton'>Leer mas</a>
+          <p>
+            Nuestra asociación <b>nació hace 15 años</b> con la misión de
+            promover el desarrollo profesional de los técnicos de celulares.
+            Creemos en la importancia de la <b>formación continua</b> y la{" "}
+            <b>actualización</b> de conocimientos. Por eso, creamos{" "}
+            <b>ASATEC Estudios Superiores (AES+)</b>, que ofrece una amplia gama
+            de cursos y especializaciones. Contamos con certificados no
+            oficiales que garantizan la <b>calidad</b> de nuestros servicios y
+            el <b>compromiso</b> con la excelencia.
+          </p>
+          <a href="/nosotros" className="boton">
+            Leer mas
+          </a>
         </div>
       </div>
-
     </div>
   );
 };
